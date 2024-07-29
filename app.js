0428ddce13bef695510f90151ea94078db3444cf
Mirko Fede <mirko@mirkoeffe.shop>
2024-07-29 17:03:58 +0200

    lab solved

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
-
+require("dotenv").config()
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const port = process.env.SERVER_PORT;
+const projects = require("./data/projects.json");
+const articles = require("./data/articles.json");
 
 
 // CREATE EXPRESS APP
@@ -13,13 +19,37 @@
 // - `express.static()` to serve static files from the `public` folder
 // - `express.json()` to parse incoming requests with JSON payloads
 // - `morgan` logger to log all incoming requests
-
+app.use(express.json());
+app.use(express.static("public"));
+app.use(morgan('dev'));
 
 
 // ROUTES
 // Start defining your routes here:
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/home.html")
+})
 
+app.get("/blog", (req, res) => {
+    res.sendFile(__dirname + "/views/blog.html")
+})
 
+app.get("/api/projects", (req, res) => {
+    res.json(projects)
+})
+
+app.get("/api/articles", (req, res) => {
+    res.json(articles)
+})
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + "/views/not-found.html")
+})
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
+
+app.listen(port, (err) => {
+    if (err) throw err;
+    console.info("Server is running on port:" + port);
+})
